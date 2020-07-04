902dace091c88957300f91f96e6cf1e07e161795
msaka73 <msaka73@gmail.com>
2020-07-04 13:12:00 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 坂本充生 on 2020/07/04.
+//  Copyright © 2020 Michio.Sakamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ InstagramTests/InstagramTests.swift
@@ -0,0 +1,34 @@
+//
+//  InstagramTests.swift
+//  InstagramTests
+//
+//  Created by 坂本充生 on 2020/07/04.
+//  Copyright © 2020 Michio.Sakamoto. All rights reserved.
+//
+
+import XCTest
+@testable import Instagram
+
+class InstagramTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
